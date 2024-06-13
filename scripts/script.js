bb4dcc7bcbdbf56169a47e4f7003fc7b207dc9fc
ChrisPM <christian.perezmtz@hotmail.com>
2024-06-13 12:09:30 -0600

    Se a añadido el 'boton' cancelar en el popup

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,37 @@ let popupModalContainer = document.querySelector(".popup");
 let buttonOpenPopUp = document.querySelector(".profile__info-edit-diseño");
 let buttonClosePopUp = document.querySelector(".popup__container-close-window");
 
+let nameDisplay = document.querySelector(".profile__info-name");
+let ocupationDisplay = document.querySelector(".profile__info-ocupation");
+let nameInput = document.getElementById("nombre");
+let ocupationInput = document.getElementById("ocupation");
+
+let formElement = document.querySelector(".popup__container");
+
 buttonOpenPopUp.addEventListener("click", function () {
   popupModalContainer.classList.add("popup__opened");
 });
 
 buttonClosePopUp.addEventListener("click", function () {
   popupModalContainer.classList.remove("popup__opened");
+
+  nameInput.value = nameDisplay.textContent;
+  ocupationInput.value = ocupationDisplay.textContent;
 });
+
+nameInput.value = nameDisplay.textContent;
+ocupationInput.value = ocupationDisplay.textContent;
+
+function handleProfileFormSubmit(evet) {
+  evet.preventDefault();
+
+  let nameValue = nameInput.value;
+  let ocupationValue = ocupationInput.value;
+
+  nameDisplay.textContent = nameValue;
+  ocupationDisplay.textContent = ocupationValue;
+
+  popupModalContainer.classList.remove("popup__opened");
+}
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
