71b8983186499718114f949a5608d83f3a73d02c
ChrisPM <christian.perezmtz@hotmail.com>
2024-07-21 21:36:38 -0600

    Se han añadido las teclas Escape en funcionamiento de los overlay

--- scripts/script.js
+++ scripts/script.js
@@ -120,8 +120,21 @@ function closePopupOnOverlayClick(popupElement) {
   popupElement.addEventListener("click", (event) => {
     if (event.target === popupElement) {
       popupElement.classList.remove("popup__opened");
+      popupElement.classList.remove("popup-image__opened");
     }
   });
+}
+
+function closePopupOnEscape(event) {
+  if (event.key === "Escape") {
+    const openPopups = document.querySelectorAll(
+      ".popup__opened, .popup-image__opened"
+    );
+    openPopups.forEach((popup) => {
+      popup.classList.remove("popup__opened");
+      popup.classList.remove("popup-image__opened");
+    });
+  }
 }
 
 // *** Listeners de Eventos ***
@@ -176,3 +189,5 @@ popupPublicImage
 closePopupOnOverlayClick(popupEditProfile);
 closePopupOnOverlayClick(popupPublicImage);
 closePopupOnOverlayClick(popupImage);
+
+document.addEventListener("keydown", closePopupOnEscape);
